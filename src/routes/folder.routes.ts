e276b7a1f696fce626fef64a161e83c6e928f009
Rey Gienaldo Jonathan <83940099+j-reygienaldo@users.noreply.github.com>
2025-05-09 18:19:06 +0700

    Add more routes and services

--- src/routes/folder.routes.ts
+++ src/routes/folder.routes.ts
@@ -1,10 +1,29 @@
 import Elysia from "elysia";
-import { getAllFolders, getSubFolder } from "../services/folder.service";
+import {
+  createFolder,
+  deleteFolder,
+  getAllFolders,
+  getParentFolder,
+  getSubFolder,
+  updateFolder,
+} from "../services/folder.service";
 
 export const folderRoutes = new Elysia({ prefix: "/folders" })
   .get("/", async () => {
     return await getAllFolders();
   })
-  .get("/:id/sub", async ({ params }) => {
-    return await getSubFolder(Number(params.id));
+  .get("/parent", async () => {
+    return await getParentFolder();
+  })
+  .post("/", async ({ body: { name, parent_id } }) => {
+    return await createFolder(name, parent_id);
+  })
+  .get("/:id/sub", async ({ params: { id } }) => {
+    return await getSubFolder(Number(id));
+  })
+  .patch("/:id", async ({ params: { id }, body: { name } }) => {
+    return await updateFolder(Number(id), name);
+  })
+  .delete("/:id", async ({ params: { id } }) => {
+    return await deleteFolder(Number(id));
   });
--- src/services/folder.service.ts
+++ src/services/folder.service.ts
@@ -10,6 +10,100 @@ export async function getAllFolders() {
     };
 }
 
+export async function getParentFolder() {
+  const result = await db.folder.findMany({
+    where: { parent_id: null, isDeleted: false || null },
+    orderBy: { id: "asc" },
+  });
+
+  return {
+    message: "Succesfully fetch data!",
+    data: result,
+  };
+}
+
 export async function getSubFolder(parent_id: number) {
-  return await db.folder.findMany({ where: { parent_id } });
+  const result = await db.folder.findMany({
+    where: { parent_id, isDeleted: false || null },
+    orderBy: { id: "asc" },
+  });
+
+  if (result)
+    return {
+      message: "Succesfully fetch data!",
+      data: result,
+    };
+}
+
+export async function createFolder(name: string, parent_id?: number) {
+  if (parent_id !== null || parent_id !== "") {
+    const findParentId = await db.folder.findFirst({
+      where: {
+        parent_id,
+      },
+    });
+
+    if (!findParentId)
+      return {
+        message: "Folder parent_id not found!",
+      };
+  }
+
+  const result = await db.folder.create({
+    data: {
+      name,
+      parent_id: parent_id,
+    },
+  });
+
+  return {
+    message: "Succesfully add new folder!",
+    data: result,
+  };
+}
+
+export async function updateFolder(id: number, name?: string) {
+  const findFolder = await db.folder.findFirst({
+    where: { id },
+  });
+
+  if (!findFolder) {
+    return {
+      message: "Folder not found!",
+    };
+  }
+
+  const updatedFolderName = await db.folder.update({
+    where: { id: findFolder.id },
+    data: { name },
+  });
+
+  return {
+    message: "Succesfully update folder name!",
+    data: updatedFolderName,
+  };
+}
+
+export async function deleteFolder(id: number) {
+  const findFolder = await db.folder.findFirst({
+    where: { id },
+  });
+
+  if (!findFolder) {
+    return {
+      message: "Folder not found!",
+    };
+  }
+
+  const deletedFolder = await db.folder.update({
+    where: { id },
+    data: {
+      isDeleted: true,
+      deleted_at: new Date().toISOString(),
+    },
+  });
+
+  return {
+    message: `Succesfully delete folder with id: ${deletedFolder.id}`,
+  };
 }
