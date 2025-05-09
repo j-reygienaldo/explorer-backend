c3bcb4ddc1e5dc1131ecad23bcdb2072df4ad613
Rey Gienaldo Jonathan <83940099+j-reygienaldo@users.noreply.github.com>
2025-05-10 01:36:18 +0700

    Add cors option

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import { Elysia } from "elysia";
+import cors from "@elysiajs/cors";
 import { folderRoutes } from "./routes/folder.routes";
 
 const app = new Elysia({ prefix: "/api/v1" });
 
 app.use(folderRoutes);
 
-app.listen(3000);
+app.use(cors());
+
+app.listen(4000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
